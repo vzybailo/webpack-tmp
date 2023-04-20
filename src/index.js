6ebd42b51cdd927c09efdaa26620b2115c36d613
MAXBIT\v.zybailo <v.zybailo@jetmail.cc>
2023-04-20 12:33:32 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './index.html';
+import './styles.scss';
+import {sum} from './modules/calc'
+
+console.log(sum(5, 4))
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const mode = process.env.NODE_ENV || 'development';
+
+const devMode = mode === 'development';
+
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    devServer: {
+        open: true,
+        hot: true,
+    },
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: 'index.[contenthash].js',
+        assetModuleFilename: 'assets/[name][ext]'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src', 'index.html')
+        }),
+        new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+    })],
+    module: {
+        rules: [
+          {
+            test: /\.html$/i,
+            loader: "html-loader",
+          },
+          {
+            test: /\.(c|sa|sc)ss$/i,
+            use: [
+                devMode ? "style-loader" : MiniCssExtractPlugin.loader, 
+                "css-loader",
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        postcssOptions: {
+                            plugins: [require('postcss-preset-env')]
+                        }
+                    }
+                },
+                "sass-loader"
+            ],
+          },
+          {
+            test: /\.woff2?$/,
+            type: 'asset/resource',
+            generator: {
+                filename: 'fonts/[name][ext]'
+            }
+          },
+          {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: "defaults" }]
+                ]
+              }
+            }
+          }
+        ],
+      },
+}
